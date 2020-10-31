059a8308be9a9c3ac84efdc5851d9c40c2c65fe1
hortencia718 <hortenciaecisneros@gmail.com>
2020-10-30 20:44:30 -0400

    browserrouter issue

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
+ import ReactDOM from 'react-dom';
+import './index.css';
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
-
+import Login from './Components/login'
 
 class App extends React.Component {
 
-
+// state={
+//   username: " ",
+//   email:" ",
+  
+// }
 
 render (){
 
   return(
     <div className="App">
-      {/* <Header title="color me a Room"/> */}
       <h1 className="rainbow-text">Color me a Room</h1>
-      {/* <Login /> */}
+      <Login />
      
     </div>
   )
